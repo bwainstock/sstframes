1208a79d476b412406decf7d754b988f006f57a7
Barry Wainstock <barrywainstock@gmail.com>
2016-07-02 22:09:17 +0000

    setup.py for single file exe

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from distutils.core import setup
-import py2exe
+import py2exe, sys, os
 
-setup(console=['sst.py'])
+sys.argv.append('py2exe')
+
+setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    windows = [{'script': "sst.py"}],
+    zipfile = None,
+)
